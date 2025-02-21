de82e0d08984a5792d0da54df850a48255afa246
Abdulrahman Almousa <99354961+amm-2001@users.noreply.github.com>
2025-02-21 12:57:44 +0300

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -62,57 +62,62 @@ staging_songs_table_create = ("""
 
 songplay_table_create = ("""
     CREATE TABLE IF NOT EXISTS songplay (
-        songplay_id INT IDENTITY(0,1),
-        start_time TIMESTAMP,
-        user_id INT,
+        songplay_id INT Not NULL IDENTITY(0,1),
+        start_time TIMESTAMP NOT NULL,
+        user_id INT Not NULL,
         level VARCHAR,
-        song_id VARCHAR,
-        artist_id VARCHAR,
-        session_id INT,
+        song_id VARCHAR NOT NULL,
+        artist_id VARCHAR NOT NULL,
+        session_id INT NOT NULL,
         location TEXT,
-        user_agent TEXT
+        user_agent TEXT,
+        PRIMARY KEY(songplay_id)
     )
 """)
 
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS users (
-        user_id INT,
-        first_name VARCHAR,
-        last_name VARCHAR,
-        gender CHAR(1),
-        level VARCHAR
+        user_id INT Not NULL,
+        first_name VARCHAR NOT NULL,
+        last_name VARCHAR NOT NULL,
+        gender CHAR(1) NOT NULL,
+        level VARCHAR,
+        PRIMARY KEY(user_id)
     )
 """)
 
 song_table_create = ("""
     CREATE TABLE IF NOT EXISTS song (
-        song_id VARCHAR,
+        song_id VARCHAR NOT NULL,
         title VARCHAR,
-        artist_id VARCHAR,
+        artist_id VARCHAR NOT NULL,
         year INT,
-        duration FLOAT
+        duration FLOAT,
+        PRIMARY KEY(song_id)
     )
 """)
 
 artist_table_create = ("""
     CREATE TABLE IF NOT EXISTS artist (
-        artist_id VARCHAR,
+        artist_id VARCHAR NOT NULL,
         name VARCHAR,
         location TEXT ,
         latitude FLOAT ,
-        longitude FLOAT
+        longitude FLOAT,
+        PRIMARY KEY(artist_id)
     )
 """)
 
 time_table_create = ("""
     CREATE TABLE IF NOT EXISTS time (
-        start_time TIMESTAMP,
+        start_time TIMESTAMP NOT NULL,
         hour INT,
         day INT,
         week INT,
         month INT,
         year INT,
-        weekday VARCHAR
+        weekday VARCHAR,
+        PRIMARY KEY(start_time)
     )
 """)
 
